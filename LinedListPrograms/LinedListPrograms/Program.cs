58e60a54cb2411d434c464e29e24e3ae0edfdd85
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-14 20:23:52 +0530

    [Anjaneya]Added:welcome message for linked list

---
+++ LinedListPrograms/LinedListPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinedListPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to DataStructure linked list program");
+        }
+    }
+}
